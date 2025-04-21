2863fdf8ef293f01c3be0b53a5137bec7b815329
AP2328 <ysamscholar@gmail.com>
2025-04-21 09:21:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Placeholder for future enhancements like search, cart, filters
 console.log("Welcome to Fashora!");
+
+
+-/1---------------
+
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
