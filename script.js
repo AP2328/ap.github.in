b2d42a72708a087640ff5b941adfa46a2cd115f1
AP2328 <ysamscholar@gmail.com>
2025-04-21 11:12:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-// Placeholder for future enhancements like search, cart, filters
-console.log("Welcome to Fashora!");
-
-
--/1---------------
-
-const menuToggle = document.getElementById('menuToggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
-
-
--2/------------
-
 const carousel = document.getElementById('carousel');
 const leftBtn = document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
 
 // Scroll amount
-const scrollAmount = 220; // Adjust this value as needed
+const scrollAmount = 220;
 
 // Scroll to the left
 leftBtn.addEventListener('click', () => {
